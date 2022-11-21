aea873063dc9851859f0e99d848a39079799265c
sahilpabale <dev.sahilpabale@gmail.com>
2022-11-21 16:32:57 +0530

    added resume

--- components/About.tsx
+++ components/About.tsx
@@ -28,7 +28,7 @@ export const About = () => {
         I use a variety of technologies like <b>Node.js</b>, <b>Typescript</b>,{" "}
         <b>JavaScript</b>, <b>Rust</b>, <b>Java</b>, and databases like{" "}
         <b>MySQL</b>, <b>PostgreSQL</b>, <b>MongoDB</b>. I have knack for APIs
-        and systems development with a tint of <b>developer operaitions.</b>
+        and systems development with a tint of <b>developer operations.</b>
       </Text>
       <Text style={{ marginBottom: `${TEXT_MARGIN_BOTTOM}rem` }}>
         I&apos;m also very passionate about systems and drone programming and
